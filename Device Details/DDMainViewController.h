89deb21bd9c4a3107689a1b8aa48c037762d7d66
Robert-Yi-Jiang <robert.yi.jones@gmail.com>
2014-01-02 22:18:49 +1100

    Update: Add iAd banner

---
+++ Device Details/DDMainViewController.h
@@ -0,0 +1,14 @@
+//
+//  DDMainViewController.h
+//  Device Details
+//
+//  Created by Jingxi & Yi on 2/01/2014.
+//  Copyright (c) 2014 Robert Yi Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <iAd/iAd.h>
+
+@interface DDMainViewController : UIViewController <ADBannerViewDelegate>
+
+@end
---
+++ Device Details/DDMainViewController.m
@@ -0,0 +1,38 @@
+//
+//  DDMainViewController.m
+//  Device Details
+//
+//  Created by Jingxi & Yi on 2/01/2014.
+//  Copyright (c) 2014 Robert Yi Jiang. All rights reserved.
+//
+
+#import "DDMainViewController.h"
+
+@interface DDMainViewController ()
+
+@end
+
+@implementation DDMainViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
