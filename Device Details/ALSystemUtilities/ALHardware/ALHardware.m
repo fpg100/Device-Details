b9ffe7f1cf46b6e6f42b3eac8189d6b1ff976c5f
Robert Yi Jiang <robert.yi.jones@gmail.com>
2015-02-18 12:06:48 +1100

    Update third party APIs

--- Device Details/ALSystemUtilities/ALHardware/ALHardware.m
+++ Device Details/ALSystemUtilities/ALHardware/ALHardware.m
@@ -90,11 +90,7 @@
         [result isEqualToString:@"iPad2,6"]     ||
         [result isEqualToString:@"iPad2,7"])        type = @"iPad Mini";
     if ([result isEqualToString:@"iPad4,4"]     ||
-        [result isEqualToString:@"iPad4,5"]     ||
-        [result isEqualToString:@"iPad4,6"])        type = @"iPad Mini Retina";
-    if ([result isEqualToString:@"iPad4,7"]     ||
-        [result isEqualToString:@"iPad4,8"]     ||
-        [result isEqualToString:@"iPad4,9"])        type = @"iPad Mini 3";
+        [result isEqualToString:@"iPad4,5"])        type = @"iPad Mini Retina";
     if ([result isEqualToString:@"iPhone6,1"]   ||
         [result isEqualToString:@"iPhone6,2"])      type = @"iPhone 5s";
     if ([result isEqualToString:@"iPhone5,3"]   ||
