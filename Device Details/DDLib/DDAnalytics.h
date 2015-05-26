0b5f58703664e2f2c92e541799bf90418bdcc01c
Robert Yi Jiang <robert.yi.jones@gmail.com>
2015-05-26 16:38:31 +1000

    Update: Half way to fetch MAC address. But it will rejected by App Store.

---
+++ Device Details/DDLib/DDAnalytics.h
@@ -0,0 +1,13 @@
+//
+//  DDAnalytics.h
+//  Device Details
+//
+//  Created by Robert Yi Jiang on 26/5/2015.
+//  Copyright (c) 2015 Robert Yi Jiang. All rights reserved.
+//
+
+@interface DDAnalytics : NSObject
+
++(NSString*)ip2mac:(char*)ip;
+
+@end
